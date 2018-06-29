2c07bf5bbb72abfd0876d36061bd2f5ae6b4dc98
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-06-28 22:02:32 -0400

    making app load without storyboard using window property of AppDelegate

--- LBTA_Twitter/ViewController.swift
+++ LBTA_Twitter/ViewController.swift
@@ -1,25 +1,10 @@
-//
-//  ViewController.swift
-//  LBTA_Twitter
-//
-//  Created by Aleksey Kabishau on 0628..18.
-//  Copyright © 2018 Aleksey Kabishau. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
