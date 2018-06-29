551bbb7aa506f6a8d815eb9bf7b2ae0a9a461a68
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-06-28 22:09:39 -0400

    creating new collection view controller

---
+++ LBTA_Twitter/HomeController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class HomeController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green
+        
+    }
+}
